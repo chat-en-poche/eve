11c550e09b41d1483ec15c3937c4a1afe240faa8
Savina Daria <dsavina@teh-lab.ru>
2019-03-07 12:59:27 +0300

    First commit.

---
+++ src/ru/dsavina/eve/Aibohphobia.java
@@ -0,0 +1,29 @@
+package ru.dsavina.eve;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Aibohphobia {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.print("> ");
+
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        String phrase = br.readLine();
+
+        for (int i = phrase.length() - 1; i >= 0; i--) {
+
+            System.out.print(phrase.charAt(i));
+
+        }
+
+        System.out.println();
+
+        System.out.print("> ");
+
+    }
+}
