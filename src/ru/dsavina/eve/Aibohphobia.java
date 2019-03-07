a9b2a1c2ee552a7b8c2c74efb3358bae4707ebab
Savina Daria <dsavina@teh-lab.ru>
2019-03-07 19:18:28 +0300

    Более лучшее всё и всякие модные фичи.

--- src/ru/dsavina/eve/Aibohphobia.java
+++ src/ru/dsavina/eve/Aibohphobia.java
@@ -8,16 +8,40 @@ public class Aibohphobia {
 
     public static void main(String[] args) throws IOException {
 
-        System.out.print("> ");
-
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
 
-        String phrase = br.readLine();
+        while (true) {
 
-        for (int i = phrase.length() - 1; i >= 0; i--) {
+            System.out.println();
+            System.out.print("Пиши, блядь > ");
 
-            System.out.print(phrase.charAt(i));
+            String phrase = br.readLine();
+
+            if (phrase.equals("выход")) {
+                break;
+            }
+
+            for (int i = phrase.length() - 1; i >= 0; i--) {
+                System.out.print(phrase.charAt(i));
+            }
+
+            String stripped = phrase.toLowerCase().replaceAll("[^a-zа-я0-9]", "");
+
+            boolean palindrome = true;
+
+            for (int i = 0; i < stripped.length() / 2; i++) {
+                if (stripped.charAt(i) != stripped.charAt(stripped.length() - 1 - i)) {
+                    palindrome = false;
+                    break;
+                }
+            }
+
+            if (palindrome == true) {
+                System.out.println(" -- это палиндром.");
+            } else {
+                System.out.println(" -- это не палиндром.");
+            }
 
         }
     }
